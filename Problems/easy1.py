99621e16dbde5eabf1cd2b1380973ae4a6c24293
Blake Pozolo <btpozolo@umich.edu>
2024-04-29 08:41:23 -0700

    easy1 partially complete

--- Problems/easy1.py
+++ Problems/easy1.py
@@ -99,3 +99,72 @@ string = "What's up doc?"
 print(word_sizes(string) == {6: 1, 2: 1, 4: 1})
 
 print(word_sizes('') == {})
+
+# Letter counter pt 2
+
+def word_sizes(string):
+    words = string.split()
+    my_dict = dict()
+
+    def remove_non_alpha(string):
+        clean_str = ""
+        for char in string:
+            if char.isalpha():
+                clean_str += char
+        return clean_str
+
+
+    for word in words:
+        clean_word = remove_non_alpha(word)
+        length = len(clean_word)
+
+        if my_dict.get(length):
+            my_dict[length] += 1
+        else:
+            my_dict[length] = 1
+    
+    return my_dict
+
+
+# All of these examples should print True
+
+string = 'Four score and seven.'
+print(word_sizes(string) == {4: 1, 5: 2, 3: 1})
+
+string = 'Hey diddle diddle, the cat and the fiddle!'
+print(word_sizes(string) == {3: 5, 6: 3})
+
+string = 'Humpty Dumpty sat on a w@ll'
+print(word_sizes(string) == {6: 2, 3: 2, 2: 1, 1: 1})
+
+string = "What's up doc?"
+print(word_sizes(string) == {5: 1, 2: 1, 3: 1})
+
+print(word_sizes('') == {})
+
+# Letter Swap
+
+def swap(sentence):
+    words = sentence.split()
+    swapped = []
+    
+    for word in words:
+        # Get first and last chars
+        first_char = word[0]
+        last_char = ""
+        if word[1:]:
+            last_char = word[1:][-1]
+
+        # remove first and last chars (splice)
+        middle = word[1:len(word)-1]
+
+        # concat word with first and last swapped
+        swap = last_char + middle + first_char
+        swapped.append(swap)
+
+    return " ".join(swapped)
+
+print(swap('Oh what a wonderful day it is')
+      == "hO thaw a londerfuw yad ti si")  # True
+print(swap('Abcde') == "ebcdA")            # True
+print(swap('a') == "a")                    # True
